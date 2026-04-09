09bf71c88ddf7a8520444b659e18b5115f9bacb7
Rob Sanderson <azaroth@gmail.com>
2026-04-09 17:04:18 -0400

    File system based storage for results

--- chai/storage.py
+++ chai/storage.py
@@ -1,3 +1,7 @@
+import os
+
+import ujson as json
+
 from .core import Component, Result
 
 
@@ -5,7 +9,7 @@ class Storage(Component):
     """Take the input and store it somewhere"""
 
     def build_json(self, input: Result):
-        return input.to_json()
+        return input.to_json(recurse=False)
 
     def _process(self, input: Result) -> Result:
         # Do persistence here
@@ -17,6 +21,33 @@ class FileSystemStorage(Storage):
         super().__init__(tree, workflow, parent)
         if "directory" not in self.settings:
             self.settings["directory"] = "results"
+        dn = self.settings["directory"]
+        if not os.path.exists(dn):
+            os.makedirs(dn, exist_ok=True)
+
+    def _process(self, input: Result) -> Result:
+        """Write the result to the filesystem according to the processor id"""
+        if input.processor is not None:
+            # Store in directory per processor
+            base = os.path.join(self.settings["directory"], input.processor.id)
+        else:
+            base = os.path.join(self.settings["directory"], "base")
+        if not os.path.exists(base):
+            os.makedirs(base, exist_ok=True)
+        # Now make a pair-tree
+        pair = os.path.join(base, input.id[0:2], input.id[2:4])
+        if not os.path.exists(pair):
+            os.makedirs(pair, exist_ok=True)
+        fn = os.path.join(pair, f"{input.id}.json")
+        if os.path.exists(fn):
+            # make a new version
+            vn = 1  # FIXME: Make this the count of files with this name
+            fn = f"{fn}.{vn}"
+        js = self.build_json(input)
+        with open(fn, "w") as fh:
+            json.dump(js, fh)
+
+        return input
 
 
 class PostgresStorage(Storage):
